0674d8a6e6df82bebe4d57c944df25d406528ac9
Joe Vandzura <vandzura.joe.e@gmail.com>
2023-07-17 09:38:53 -0500

    first commit

---
+++ src/main/java/com/gradeez/gradeezapp/GradeEzAppApplication.java
@@ -0,0 +1,13 @@
+package com.gradeez.gradeezapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradeEzAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradeEzAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gradeez/gradeezapp/GradeEzAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gradeez.gradeezapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradeEzAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
