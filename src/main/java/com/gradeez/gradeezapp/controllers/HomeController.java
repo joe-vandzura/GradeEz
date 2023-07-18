15d468ca072468ba108e4ccd5e5dd5c6e6c95fb7
Joe Vandzura <vandzura.joe.e@gmail.com>
2023-07-18 15:52:23 -0500

    added oauth2 stuff w/ google login

--- src/main/java/com/gradeez/gradeezapp/controllers/HomeController.java
+++ src/main/java/com/gradeez/gradeezapp/controllers/HomeController.java
@@ -1,14 +1,23 @@
 package com.gradeez.gradeezapp.controllers;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("/")
 public class HomeController {
 
-    @GetMapping("/")
-    public String loginPage() {
-        return "index";
+//    @GetMapping("/")
+//    public String loginPage() {
+//        return "index";
+//    }
+
+    @GetMapping
+    public OAuth2User returnToken(OAuth2AuthenticationToken token) {
+        return token.getPrincipal();
     }
 
 }
