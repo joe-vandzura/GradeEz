d5d2e9ac788ca54ce1c29e8dc3954973368851e5
Joe Vandzura <vandzura.joe.e@gmail.com>
2023-08-09 08:58:18 -0500

    stripped jwt/oauth stuff and replacing with spring security configuration

--- src/main/java/com/gradeez/gradeezapp/controllers/HomeController.java
+++ src/main/java/com/gradeez/gradeezapp/controllers/HomeController.java
@@ -1,25 +1,14 @@
 package com.gradeez.gradeezapp.controllers;
 
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-//@RestController
-//@RequestMapping("/")
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String loginPage() {
+    public String showHomePage() {
         return "index";
     }
-
-//    @GetMapping
-//    public OAuth2User returnToken(OAuth2AuthenticationToken token) {
-//        return token.getPrincipal();
-//    }
 
 }
