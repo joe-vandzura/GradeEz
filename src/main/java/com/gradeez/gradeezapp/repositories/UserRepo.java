4c28de47da64ea510f1ff3254fab01ed6ea34e71
Joe Vandzura <vandzura.joe.e@gmail.com>
2023-08-10 11:45:10 -0500

    committing changes

--- src/main/java/com/gradeez/gradeezapp/repositories/UserRepo.java
+++ src/main/java/com/gradeez/gradeezapp/repositories/UserRepo.java
@@ -1,11 +1,10 @@
 package com.gradeez.gradeezapp.repositories;
 
+import com.gradeez.gradeezapp.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Long> {
-
-    Optional<User> findByEmail(String email);
+public interface UserRepo extends JpaRepository<User, Long> {
 
 }
